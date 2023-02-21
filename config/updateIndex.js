c71f20cdf6420f2bed06887150f292140afed91f
Guillaume Fradin <guillaumefradin@gmail.com>
2023-02-21 23:48:23 +0000

    update deploy script

--- config/updateIndex.js
+++ config/updateIndex.js
@@ -1,14 +1,18 @@
 import * as fs from "fs";
-import * as path from "path";
 
-const divApp = `<div id="app"></div>`;
+const LINE_APP = `<div id="app"></div>`;
+const LINE_HEAD_END = `</head>`;
+
 const filePath = "./dist/index.html";
 const content = fs.readFileSync(filePath, "utf-8");
 const lines = content.split("\n");
 const newLines = lines.map((line) => {
-	if (line.includes(divApp)) {
+	if (line.includes(LINE_APP)) {
 		return `${line}
 		<%= yield %>`;
+	} else if (line.includes(LINE_HEAD_END)) {
+		return `		<%= landing_page_layout_js %>
+	${line}`;
 	} else {
 		return line;
 	}
