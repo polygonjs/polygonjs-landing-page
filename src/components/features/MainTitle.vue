4b82238078863eb8ff1fafa22b399f462a8b22cf
Guillaume Fradin <guillaumefradin@gmail.com>
2023-05-30 21:51:08 +0100

    login link fix

--- src/components/features/MainTitle.vue
+++ src/components/features/MainTitle.vue
@@ -35,13 +35,13 @@
 			class="block lg:inline whitespace-nowrap leading-tight border-b-4 border-rose-700 hover:bg-rose-700 dark:border-rose-700 dark:hover:bg-rose-700"
 			>Dashboard</a
 		>
-		<span v-if="loggedIn" class="hidden lg:inline text-sm">&bull;</span>
 		<a
-			v-if="loggedIn"
+			v-if="!loggedIn"
 			:href="loginRoute"
 			class="block lg:inline whitespace-nowrap leading-tight border-b-4 border-fuchsia-400 hover:bg-fuchsia-400 dark:border-fuchsia-700 dark:hover:bg-fuchsia-700"
 			>Login</a
 		>
+		<span v-if="!loggedIn" class="hidden lg:inline text-sm">&bull;</span>
 		<a
 			v-if="!loggedIn"
 			:href="signupRoute"
